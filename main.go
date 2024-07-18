da5f9075e510df32a7b51052973a7e0649cdff80
Matheus Valle <55810334+mtheusvalle@users.noreply.github.com>
2024-07-18 19:52:50 -0300

    feat: add routes

--- main.go
+++ main.go
@@ -4,10 +4,4 @@ import "github.com/mtheusvalle/gopportunities/router"
 
 func main() {
 	router.Initialize()
-
-	r := router.Teste{
-		Name: "Teste",
-	}
-
-	print(r)
 }
--- router/router.go
+++ router/router.go
@@ -4,16 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Teste struct {
-	Name string
-}
-
 func Initialize() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080")
+	router := gin.Default()
+	initializeRoutes(router)
+	router.Run(":8080")
 }
