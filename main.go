e3be6160c14e3c1e77e92b7a36a1331a01df7adc
Matheus Valle <55810334+mtheusvalle@users.noreply.github.com>
2024-07-22 21:38:44 -0300

    Revert "feat: Criando logs"
    This reverts commit cd5869561dc45e9c6e536638cc2d3df15ca0fef3.

--- main.go
+++ main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"github.com/mtheusvalle/gopportunities/config"
-	"github.com/mtheusvalle/gopportunities/router"
-)
-
-var (
-	logger config.Logger
-)
+import "github.com/mtheusvalle/gopportunities/router"
 
 func main() {
-	logger = *config.GetLogger("main")
-	// Initialize configs
-	err := config.Init()
-	if err != nil {
-		logger.Error("config initialization error: ", err)
-		return
-	}
-
-	// Initialize router
 	router.Initialize()
 }
--- router/routes.go
+++ router/routes.go
@@ -1,17 +1,47 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
-	"github.com/mtheusvalle/gopportunities/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", handler.GetOpeningHandler)
-		v1.GET("/opening/:id", handler.GetOpeningByIDHandler)
-		v1.POST("/opening", handler.CreateOpeningHandler)
-		v1.PUT("/opening/:id", handler.UpdateOpeningHandler)
-		v1.DELETE("/opening/:id", handler.DeleteOpeningHandler)
+		// GET
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Opening",
+			})
+		})
+
+		// GET BY ID
+		v1.GET("/opening/:id", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Opening by ID",
+			})
+		})
+
+		// POST
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "POST Opening",
+			})
+		})
+
+		// PUT
+		v1.PUT("/opening/:id", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "PUT Opening",
+			})
+		})
+
+		// DELETE
+		v1.DELETE("/opening/:id", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "DELETE Opening",
+			})
+		})
 	}
 }
